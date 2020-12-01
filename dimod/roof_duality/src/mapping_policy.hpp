8f2df3c6693253e1d20e20b1c1e208c135b26f4b
Anil Mahmud <amahmud@dwavesys.com>
2020-12-01 14:37:10 -0800

    new mapping

--- dimod/roof_duality/src/mapping_policy.hpp
+++ dimod/roof_duality/src/mapping_policy.hpp
@@ -20,28 +20,31 @@
 #ifndef IMPLICATION_NETWORK_MAPPING_POLICY_HPP_INCLUDED
 #define IMPLICATION_NETWORK_MAPPING_POLICY_HPP_INCLUDED
 
-// PLEASE DO NOT MAKE AN INTERFACE FOR MAPPING POLICY. VIRTUAL DISPATCH WILL ADD
+// PLEASE DO NOT MAKE AN INTERFACE FOR THE MAPPER OBJECTS. VIRTUAL DISPATCH WILL ADD
 // TO THE ALREADY EXISTING PENALTY OF USING THIS MAPPING CLASS. WE CREATED THIS
 // CLASS SINCE WE CAN HAVE PERFORMANCE ADVANTAGE WITH ALTERNATE MAPPING WHILE IT
 // IS EASIER TO DEBUG WITH SEQUENTIAL MAPPING, BUT IT IS HARD TO SHIFT BETWEEN
 // THEM WITHOUT A DEDICATED CLASS.
 
-// If there are n variables in the Quobo, the posiform will have 2 * ( n + 1 )
+// These mapper objects control variable to vertex mapping. Please do not manually map
+// them even though it may seem easy, since we may change the mapping by these
+// functions later. The assertions should be changed if the mapping is
+// changed. If there are n variables in the Quobo, the posiform will have 2 * ( n + 1 )
 // variables, each variable with its complement and a root, X_0/X_source and its
 // complement. Here we treat variable 0-n-1 as the original n Qubo variables and
-// variable n as X_0/X_source. Variable n+1 to 2n+1 are their complements, thus
-// variable v has its complement as (v + n + 1). This method of mapping is easy
+// variable n as X_0/X_source.
+
+// In sequential mapping the variables 0-n-1 map to vertices 0-n-1. The source
+// variable maps to vertex n. Then the vertices n+1 to 2*n + 1 correspond to the
+// complements of vertices 0 - n sequentially. This method of mapping is easy
 // for debugging purpose but does not keep the ordering that may be in the qubo
-// as regards to the biases and the corresponding variables.
+// as regards to the biases which may be sorted based on the connecting
+// variables.
 class sequentialMapper {
 public:
   sequentialMapper(int num_variables) : _num_variables(num_variables) {}
 
   sequentialMapper() {}
-  // These functions control variable to vertex mapping. Do not manually map
-  // them even though it may seem easy, since we may change the mapping by these
-  // functions later. The assertions should be changed if the mapping is
-  // changed.
 
   inline int source() { return _num_variables; }
 
@@ -69,6 +72,44 @@ public:
   }
 
   inline bool complement_maintains_order() { return false; }
+
+private:
+  int _num_variables;
+};
+
+// The oddEvenMapper maps variable x to vertex 2 * x and its complement is 2 * x
+// + 1. Same as before we consider variable n to be the source.
+class evenOddMapper {
+public:
+  oddEvenMapper(int num_variables) : _num_variables(num_variables) {}
+
+  oddEvenMapper() {}
+
+  inline int source() { return 2 * _num_variables; }
+
+  inline int sink() { return 2 * _num_variables + 1; }
+
+  inline int complement(int vertex) {
+    assert((vertex <= 2 * _num_variables + 1) && (vertex >= 0));
+    return (vertex % 2) ? (vertex - 1) : (vertex + 1);
+  }
+
+  inline int non_complemented_vertex(int vertex) {
+    assert((vertex <= 2 * _num_variables + 1) && (vertex >= 0));
+    return (vertex % 2) ? (vertex - 1) : (vertex);
+  }
+
+  inline int vertex_to_variable(int vertex) {
+    assert((vertex < _num_variables) && (vertex >= 0));
+    return vertex / 2;
+  }
+
+  inline int variable_to_vertex(int variable) {
+    assert((variable < _num_variables) && (variable >= 0));
+    return variable * 2;
+  }
+
+  inline bool complement_maintains_order() { return true; }
 
 private:
   int _num_variables;
